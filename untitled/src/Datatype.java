6d449183fa5e161bc58b66adcfed3e89f1b172ca
Amit11614736 <amit11614736@gmail.com>
2025-10-17 00:13:22 +0530

    adding datatype code

---
+++ untitled/src/Datatype.java
@@ -0,0 +1,24 @@
+public class Datatype {
+    public static void main(String args[])
+    {
+        byte standard=12;
+        short otp=1212;
+        int age=15;
+        long phone=4545121265l;
+        float cgpa=8.8f;
+        double salary=842203d;
+        boolean examPassed=true;
+        char gender='M';
+        String name="Hemant soren";
+        System.out.println(standard);
+        System.out.println(otp);
+        System.out.println(age);
+        System.out.println(cgpa);
+        System.out.println(phone);
+        System.out.println(salary);
+        System.out.println(examPassed);
+        System.out.println(gender);
+        System.out.println(name);
+
+    }
+}
--- untitled/src/One.java
+++ untitled/src/One.java
@@ -1,4 +1,5 @@
 public class One {
-    public static void main(String args[])
-    {
-        System.out.println("Hurricane")
+    public static void main(String args[]) {
+        System.out.println("Hurricane");
+    }
+}
