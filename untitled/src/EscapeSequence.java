765283d27cb83e6a0d3c944b11c5c3e137e750c4
Amit11614736 <amit11614736@gmail.com>
2025-10-17 12:05:22 +0530

    escape sequence adding

---
+++ untitled/src/EscapeSequence.java
@@ -0,0 +1,18 @@
+public class EscapeSequence {
+
+        public static void main(String args[]) {
+            System.out.println("date \t time");// add space like the tab button does
+            System.out.println(" there are \b5 main topic in OOps");// \b whenever we add it act as a backspace, at the place where we add \b it act as backspace :it delets a gap or word
+            System.out.println("villan and heros are same difference is \nthey act as different character");
+            // \n insert new line
+            System.out.println("i have no \'idea' what it \"does\"");
+// for single quote and double quote
+
+            System.out.println(" dj sounds \\loud");// to insert backslash character
+
+        }
+    
+
+
+
+}
