361c8565dfd1c6e54084512ad4abef8a37ca4dca
Amit11614736 <amit11614736@gmail.com>
2025-11-02 12:32:39 +0530

    arithmetic optr p

---
+++ untitled/src/ArithmeticOptr.java
@@ -0,0 +1,17 @@
+public class ArithmeticOptr {
+    public static void main(String[] args) {
+        int a =92;
+        int b=6;
+
+        int add=a+b;
+        System.out.println(add);
+        int sub=a-b;
+        System.out.println(sub);
+        int mul=a*b;
+        System.out.println(mul);
+        int div=a/b;
+        System.out.println(div);
+        int mod=a%b;
+        System.out.println(mod);
+    }
+}
