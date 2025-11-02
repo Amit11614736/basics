d43420fa72458b0429f0d8c5aa2aa9327340195e
Amit11614736 <amit11614736@gmail.com>
2025-11-02 12:55:36 +0530

    practice set 9

---
+++ untitled/src/ProductOfFloatingNo.java
@@ -0,0 +1,7 @@
+public class ProductOfFloatingNo {
+    public static void main(String[] args) {
+        float no1=38.20f;
+        float no2=53.40f;
+        System.out.println(no1*no2);
+    }
+}
